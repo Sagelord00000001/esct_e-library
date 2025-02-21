a0f4f0b4e78d633995a92e777be7cd44c714f894
Chukwuebuka <emmanuelmrchukwuebuka@gmail.com>
2025-02-21 16:35:08 +0100

    Responsive Commits

--- components/FacultyCard.jsx
+++ components/FacultyCard.jsx
@@ -1,15 +1,3 @@
-// import Link from "next/link";
-
-// export default function FacultyCard({ title, description, route }) {
-//   return (
-//     <Link href={route}>
-//       <div className="bg-white border border-gray-200 shadow-md rounded-xl p-6 h-40 flex flex-col justify-between hover:shadow-lg transition">
-//         <h3 className="text-lg font-semibold text-blue-700">{title}</h3>
-//         <p className="text-gray-600 text-sm">{description}</p>
-//       </div>
-//     </Link>
-//   );
-// }
 import Link from "next/link";
 
 export default function FacultyCard({ title, description, route }) {
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,34 +1,3 @@
-// 'use client'
-
-// import Image from 'next/image';
-// import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
-// import React from 'react'
-
-// function Header() {
-//     const {user}=useKindeBrowserClient();
-
-//   return (
-//     <div className='bg-blue-700 text-white p-4 flex justify-between items-center'>
-//         <div>
-//         <h1 className="text-xl font-bold">ESCT e-Library</h1>
-//         </div>
-//         <div className=' flex items-center gap-3 ' >
-//           <div>
-//             <h2 className='text-xs font-bold'>{user?.given_name} {user?.family_name}</h2>
-//             </div>
-//             <Image
-//                 src={user?.picture || '/default-avater.png'}
-//                 width={35}
-//                 height={35}
-//                 alt='user'
-//                 className=' rounded-full'
-//             />
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Header
 'use client'
 
 import Image from 'next/image';
