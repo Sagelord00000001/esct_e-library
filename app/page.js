62124818952ef90e30eedac04066de42c3463b5a
Chukwuebuka <emmanuelmrchukwuebuka@gmail.com>
2025-03-05 08:45:08 +0100

    updated next js

--- app/page.js
+++ app/page.js
@@ -1,32 +1,8 @@
-// 'use client'
-// import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
-// import Image from "next/image";
-// import { redirect } from "next/navigation";
-// import { useEffect } from "react";
-
-// export default function Home() {
-//   useEffect(()=>{
-//     redirect('/api/auth/login?post_login_redirect_url=/dashboard')
-//   },[])
-//   return (
-//     <div>
-//       <LoginLink>Sign in</LoginLink>
-//       <RegisterLink>Sign up</RegisterLink>
-//     </div>
-//   );
-// }
-
-
 'use client'
 import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
-import Image from "next/image";
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  // useEffect(()=>{
-  //   redirect('/api/auth/login?post_login_redirect_url=/dashboard')
-  // },[])
+
   return (
     <div>
 <LoginLink postLoginRedirectURL="/dashboard">Sign in</LoginLink>
